44d7ac564a75b60d08f184a9ad0ed11b7e4d3862
mogden16 <88002833+mogden16@users.noreply.github.com>
2022-01-17 13:18:21 -0500

    Add files via upload

--- updatedmain.py
+++ updatedmain.py
@@ -17,9 +17,12 @@ import numpy as np
 from tqdm import tqdm
 from dotenv import load_dotenv
 
+
+
 load_dotenv(dotenv_path=f"{os.path.abspath(os.path.dirname(__file__))}/config.env")
 
 num_of_iterations_perAgg=int(os.getenv('num_of_iterations_perAgg'))
+agg_setup=os.getenv('num_of_iterations_perAgg')
 
 tfbutton_x = float(os.getenv('tfbutton_x'))
 tfbutton_y =float(os.getenv('tfbutton_y'))
@@ -56,6 +59,8 @@ class AutoExport:
         # THIS WILL BE THE AMOUNT OF TIMEFRAMES THAT YOU CYCLE THROUGH
         self.working_tf = []
         self.tradingSide = []
+        self.start_time = str(time.strftime("%m_%d_%y__%H_%M"))
+        self.timeframes = {'1':'1m', '2':'2m', '3':'3m', '5':'5m', '10':'10m', '15':'15m', '30':'30m', '60':'1hr', '120':'2hr', '480':'4hr', '24':'1d'}
 
         # ADJUST THIS TO ALLOW FOR SLOWER OR FASTER MOVEMENTS
         # DELAY MAY NEED TO BE INCREASED BASED ON INTERNET SPEED AND AGGREGATION/LENGTH OF CHARTS
@@ -66,10 +71,9 @@ class AutoExport:
         # THIS IS THE WATCHLIST FILE FROM TOS, WHICH IS FORMATED WITH THE CURRENT DATE OF WHICH DOWNLOADED
         self.watchlist_file = f"{datetime.now().strftime('%Y-%m-%d')}-watchlist.csv"
 
-        # PATH TO USERS DOCUMENTS FOLDER. THIS IS WHERE TOS DOWNLOADS THE STRATEGY REPORTS
-        self.path=(os.getenv('tos_study_path'))
-        self.picture_path=((os.getenv('dst_dir')) + str(self.working_tf)+ 'm/pics/')
-        self.new_study_path=None
+        # BASE PATHS TO FOLDERS
+        self.path=((os.getenv('tos_study_path'))+'/')
+        self.new_study_path=((os.getenv('dst_dir'))+'/'+ self.start_time + '/')
 
         # ADDED CONDITIONAL FOR THE WHILE LOOP IN START METHOD.
         # IF ERROR, ATTRIBUTE WILL BE SET TO FALSE AND WHILE LOOP WILL STOP
@@ -86,32 +90,40 @@ class AutoExport:
 
             print(error)
 
-#         # REMOVE ALL EXISTING STRATEGY REPORTS CSV FILES FROM DOCUMENTS FOLDER
-#         for file in os.listdir(export.path):
-#
-#             sep = file.split("_")
-#
-#             if file.endswith(".html"):
-#
-#                 try:
-#
-#                     os.remove(os.path.join(export.path, file))
-#
-#                 except:
-#
-#                     pass
+    def removeFiles(self):
+        # REMOVE ALL EXISTING STRATEGY REPORTS CSV FILES FROM DEFAULT TOS FOLDER
+        for file in os.listdir(self.path):
+            if file.endswith(".html"):
+                try:
+                    os.remove(os.path.join(self.path, file))
+                except:
+                    pass
+
+    def createFolders(self):
+
+        new_folder = os.makedirs(str(self.new_study_path),exist_ok=True)
+
+        for i in range(1,4):
+            os.makedirs(os.path.join(str(self.new_study_path) + str(i) +'m'),exist_ok=True)
+        for i in range(1,4):
+            os.makedirs(os.path.join(str(self.new_study_path) + str(i) + 'm/pics'),exist_ok=True)
+
 
     def getWatchlist(self):
 
         proceed = input("What TFs are you using? (1/2/3): ")
         while proceed != "DONE":
-            self.working_tf.append(proceed.upper())
+            self.working_tf.append(int(proceed))
             proceed = input("Any other TFs? Type DONE when finished: ")
 
 #         side = input("What side to you want to trade? (long/short/both): ")
 #         while side != "DONE":
 #             self.tradingSide.append(side.upper())
 #             side = input("Any other side you want to see? Type DONE when finished")
+
+        title = input("What would you like to title this study? ")
+            discord_title_push = {"content": "Starting study: " + str(title)}
+            response = requests.post((os.getenv('DISCORD_WEBHOOK')), json=discord_title_push)
 
         self.working_tf.sort()
         print("BEGINNING AUTO EXPORTS....")
@@ -122,16 +134,16 @@ class AutoExport:
     def moveHTMLFile(self, tf):
 
         # ABSOLUTE PATH TO CSV_FILES DIRECTORY
-        self.new_study_path=((os.getenv('dst_dir')) + str(tf)+ 'm/')
-        print(f"Moving files to: {self.new_study_path}")
+        tf_path=(self.new_study_path + str(tf) + 'm/')
+        print(f"Moving files to: {tf_path}")
 
-        for file in tqdm(glob.glob(self.path + '*.html')):
+        for file in tqdm(glob.glob(self.path + '/' + '*.html')):
 
             try:
 
                 # MOVES MOST RECENT STRATEGY REPORT FILE FROM DOCUMENTS FOLDER TO CSV_FILES FOLDER
 
-                shutil.move(file, self.new_study_path)
+                shutil.move(file, tf_path)
 
             except shutil.Error as e:
 
@@ -139,8 +151,9 @@ class AutoExport:
 
 
 
-    def createDATAFRAME(self):
-        files = glob.glob(self.new_study_path + '*.html')
+    def createDATAFRAME(self, tf):
+        print("working on dataframe")
+        files = glob.glob(self.new_study_path + '/' +str(tf) + 'm/*.html')
         df = pd.DataFrame()
 
         for f in tqdm(files):
@@ -154,29 +167,36 @@ class AutoExport:
         df['Trade P/L'] = df['Trade P/L'].str.replace(')', '',regex=False)
         df['Trade P/L'] = pd.to_numeric(df['Trade P/L'], errors='coerce')
 
+        df['Date/Time']=pd.to_datetime(df['Date/Time'], format='%m/%d/%y, %I:%M %p')
+        df['Avg Trade Time - mins']=df['Date/Time'].diff().dt.total_seconds()//60
+        tradetimes = df.dropna(subset=['Trade P/L']).groupby(['Strategy']).mean()['Avg Trade Time - mins']
+
         TotalP_L = df.groupby(['Strategy']).sum()['Trade P/L']
 
         CountTrades = df.groupby(['Strategy']).size()
 
         TotalP_Lcommission = TotalP_L - (CountTrades * .65)
 
-        df2 = pd.concat([TotalP_Lcommission, CountTrades], axis=1)
-        df2.columns = ['Total P/L minus Commission', 'Number of Trades']
+
+        df2 = pd.concat([TotalP_Lcommission, CountTrades, tradetimes], axis=1)
+        df2.columns = ['Total P/L minus Commission', 'Number of Trades', 'Avg Trade Time - mins']
         df2['P/L per Trade'] = round(df2['Total P/L minus Commission'] / df2['Number of Trades'], 2)
 
         df2 = df2.sort_values('P/L per Trade', ascending=False)
-        df3 = df2.head()
-        print(df3)
 
-        discord_message_to_push = {"content": df3.to_string()}
-        response = requests.post(discord_webhook, json=discord_message_to_push)
+        print(df2.head())
+
+        discord_tf_push = {"content": "Just finished "+ str(tf) + "m timeframe"}
+        response = requests.post((os.getenv('DISCORD_WEBHOOK')), json=discord_tf_push)
+        discord_message_to_push = {"content": df2.head().to_string()}
+        response = requests.post((os.getenv('DISCORD_WEBHOOK')), json=discord_message_to_push)
 
     def switchTotalTFs(self, tf):
 
-        if tf == '1':
+        if tf == 1:
             return
 
-        elif tf == '2':
+        elif tf == 2:
             pyautogui.moveTo(tfbutton_x, tfbutton_y) # ABSOLUTE
             time.sleep(self.delay)
             pyautogui.click()
@@ -186,7 +206,7 @@ class AutoExport:
             pyautogui.click()
             time.sleep(self.extended_delay)
 
-        elif tf == '3':
+        elif tf == 3:
              pyautogui.moveTo(tfbutton_x, tfbutton_y) # ABSOLUTE
              time.sleep(self.delay)
              pyautogui.click()
@@ -202,8 +222,8 @@ class AutoExport:
 #             time.sleep(self.delay)
 #             pyautogui.moveTo()
 
-    def switchAgg1(self, agg, agg2, study, tf):
-        picture_path = ((os.getenv('dst_dir')) + str(tf) + 'm/pics/' + str(study) + '.png')
+    def switchAgg1dropdown(self, agg, agg2, study, tf):
+        picture_path = (str(self.new_study_path) + str(tf) + 'm/pics/' + str(study) + '.png')
         # SHOW SETTINGS BUTTON
         pyautogui.rightClick(firstclick_x, firstclick_y) # ABSOLUTE
         time.sleep(self.delay)
@@ -222,17 +242,17 @@ class AutoExport:
             pyautogui.click()
             pyautogui.click()
             time.sleep(self.delay)
-            if tf == '1':
+            if tf == 1:
                 pyautogui.move(-53, -2)
                 time.sleep(self.delay)
                 pyautogui.click()
                 time.sleep(self.delay)
-            elif tf == '2':
+            elif tf == 2:
                 pyautogui.move(-53,21)
                 time.sleep(self.delay)
                 pyautogui.click()
                 time.sleep(self.delay)
-            elif tf == '3':
+            elif tf == 3:
                 pyautogui.move(-53,47)
                 time.sleep(self.delay)
                 pyautogui.click()
@@ -247,15 +267,15 @@ class AutoExport:
                 pyautogui.click()
                 pyautogui.click()
                 time.sleep(self.delay)
-                if tf == '1':
+                if tf == 1:
                     pyautogui.move(-53, -2)
                     pyautogui.click()
                     time.sleep(self.delay)
-                elif tf == '2':
+                elif tf == 2:
                     pyautogui.move(-53, 21)
                     pyautogui.click()
                     time.sleep(self.delay)
-                elif tf == '3':
+                elif tf == 3:
                     pyautogui.move(-53, 47)
                     pyautogui.click()
                     time.sleep(self.delay)
@@ -316,8 +336,90 @@ class AutoExport:
         pyautogui.click()
         time.sleep(self.delay)
 
+    def switchAgg1int(self, agg, agg2, study, tf):
+        picture_path = (str(self.new_study_path) + str(tf) + 'm/pics/' + str(study) + '_' + self.timeframes[agg] + '_' + self.timeframes[agg2] + '.png')
 
-    def switchAgg2(self, agg2, study):
+        # SHOW SETTINGS BUTTON
+        pyautogui.rightClick(firstclick_x, firstclick_y) # ABSOLUTE
+        time.sleep(self.delay)
+        pyautogui.move(57, 22) # RELATIVE
+        time.sleep(self.delay)
+        pyautogui.click()
+        time.sleep(self.delay)
+
+        pyautogui.moveTo(middleof_agg1_x, middleof_agg1_y)
+        time.sleep(self.delay)
+        pyautogui.click()
+        if self.path.startswith('/Users'):
+            with pyautogui.hold('command'):
+                pyautogui.press('a')
+        else:
+            with pyautogui.hold('ctrl'):
+                pyautogui.press('a')
+        time.sleep(self.delay)
+        pyautogui.write(agg)
+        time.sleep(self.delay)
+        pyautogui.moveTo(middleof_agg2_x, middleof_agg2_y)
+        time.sleep(self.delay)
+        pyautogui.click()
+        if self.path.startswith('/Users'):
+            with pyautogui.hold('command'):
+                pyautogui.press('a')
+        else:
+            with pyautogui.hold('ctrl'):
+                pyautogui.press('a')
+        pyautogui.write(agg2)
+        pyautogui.move(0,34)
+        time.sleep(self.delay)
+        pyautogui.click()
+
+        myScreenshot = pyautogui.screenshot()
+        myScreenshot.save(picture_path)
+        time.sleep(self.delay)
+        pyautogui.moveTo(okbutton_x, okbutton_y)
+        pyautogui.click()
+        time.sleep(self.extended_delay)
+
+
+        # BRING UP REPORT
+        pyautogui.rightClick(firstclick_x, firstclick_y) # ABSOLUTE
+        time.sleep(self.delay)
+        pyautogui.move(19,86)
+        pyautogui.click()
+        time.sleep(self.delay+5)
+
+        # EXPORT FILE BUTTON
+        pyautogui.moveTo(exportFile_x, exportFile_y)
+        time.sleep(self.delay)
+        pyautogui.click()
+
+        # SWITCH TO HTML
+        pyautogui.write(str(study) +'_' + self.timeframes[agg] + '_' +self.timeframes[agg2])
+        time.sleep(self.delay)
+        pyautogui.moveTo(filesofType_x, filesofType_y)
+        time.sleep(self.delay)
+        pyautogui.click()
+        pyautogui.move(10, 35) # RELATIVE
+        time.sleep(self.delay)
+        pyautogui.click()
+
+
+        # SAVE BUTTON (save strategy report to documents folder)
+        pyautogui.moveTo(savebutton_x, savebutton_y) # ABSOLUTE
+        time.sleep(self.delay)
+        pyautogui.click()
+        time.sleep(self.delay)
+
+        # CLOSE BUTTON (close out of the strategy report)
+        pyautogui.moveTo(exitstrat_x, exitstrat_y) # ABSOLUTE
+        time.sleep(self.delay)
+        pyautogui.click()
+        time.sleep(self.delay)
+
+
+
+
+    def switchAgg2dropdown(self, agg2, study):
         # SHOW SETTINGS BUTTON
         pyautogui.rightClick(firstclick_x, firstclick_y) # ABSOLUTE
         time.sleep(self.delay)
@@ -341,35 +443,72 @@ class AutoExport:
         pyautogui.click()
         time.sleep(self.delay)
 
+    def setTakeProfit(self):
+        pyautogui.moveTo(middleof_agg2_x, middleof_agg2_y)
+        time.sleep(self.delay)
+        pyautogui.click()
+        pyautogui.move(0,33)
+        time.sleep(self.delay)
+        pyautogui.click()
+        time.sleep(self.delay)
+        pyautogui.move(0,-28)
+        pyautogui.click()
+        if self.path.startswith('/Users'):
+            with pyautogui.hold('command'):
+                pyautogui.press('a')
+        else:
+            with pyautogui.hold('ctrl'):
+                pyautogui.press('a')
+        pyautogui.write(str(self.takeprofit))
+        self.takeprofit=self.takeprofit+.02
+
+
 
 
     def start(self):
 
+        self.removeFiles()
+        self.createFolders()
+
+
         study=0
 
         for tf in tqdm(self.working_tf):
-            for agg2 in tqdm(range(num_of_iterations_perAgg),leave=False):
-                for agg in tqdm(range(num_of_iterations_perAgg),leave=False):
+            self.switchTotalTFs(tf)
+            for agg2 in tqdm(self.timeframes.keys(),leave=False):
+                for agg in tqdm(self.timeframes.keys(),leave=False):
+                    if tf == 2 and agg == '1':
+                        continue
+                    if tf == 2 and agg2 == '1':
+                        continue
+                    if tf == 3 and (agg2 == '1' or agg2 == '2'):
+                        continue
+                    if tf == 3 and (agg == '1' or agg == '2'):
+                        continue
+
                     print("starting: agg",agg)
                     print("starting: agg2=",agg2)
                     print("starting: study=",study)
-
-                    self.switchAgg1(agg, agg2, study, tf)
+                    if agg_setup == "DROPDOWN":
+                        self.switchAgg1dropdown(agg, agg2, study, tf)
+                    else:
+                        self.switchAgg1int(agg, agg2, study, tf)
                     study+=1
 
-                # SEND TO DISCORD
-                discord_message_to_push = {"content": f"finished first column, changing agg2 by 1"}
-                response = requests.post(discord_webhook, json=discord_message_to_push)
-                self.switchAgg2(agg2, study)
+                # # SEND TO DISCORD
+                # discord_message_to_push = {"content": f"finished agg2: {self.timeframes[agg2]}"}
+                # response = requests.post((os.getenv('DISCORD_WEBHOOK')), json=discord_message_to_push)
+                if agg_setup == "DROPDOWN":
+                    self.switchAgg2dropdown(agg2, study)
 
             self.moveHTMLFile(tf)
-            self.createDATAFRAME()
-            self.switchTotalTFs(tf)
+            self.createDATAFRAME(tf)
+
 
 
         print('WE HAVE COMPLETED! CONGRATS!')
         discord_message_to_push = {"content": f"WE HAVE COMPLETED! CONGRATS!"}
-        response = requests.post(discord_webhook, json=discord_message_to_push)
+        response = requests.post((os.getenv('DISCORD_WEBHOOK')), json=discord_message_to_push)
         time.sleep(20)
 
 ###############
@@ -383,4 +522,3 @@ if __name__ == "__main__":
     time.sleep(2)
 
     export.getWatchlist()
-
